8255835c0de660db708abd6bb618530e5b89c203
BuntyBru <buntybru@gmail.com>
2022-12-06 14:37:57 +0530

    Module Pattern

---
+++ ModulePattern/math.js
@@ -0,0 +1,15 @@
+export const add = (x, y) => {
+  return x + y;
+};
+
+export const substract = (x, y) => {
+  return x - y;
+};
+
+export const divide = (x, y) => {
+  return x / y;
+};
+
+export const multiply = (x, y) => {
+  return x * y;
+};
